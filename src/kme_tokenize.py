52f17a46812ecb9aaa07e2443998569aed4e917d
namirinz <nathmcfc31115@gmail.com>
2021-08-03 11:01:30 +0700

    Update file

--- src/kme_tokenize.py
+++ src/kme_tokenize.py
@@ -1,46 +1,51 @@
-import tensorflow as tf
-import numpy as np
 import os
+
+import numpy as np
 import requests
+import tensorflow as tf
+import tensorflow.keras as tfk
 
-class Segmentation():
+from config import word_segmentation_model_url
+
+
+class Segmentation:
     def __init__(self):
-        url = 'https://firebasestorage.googleapis.com/v0/b/ysc-kme-25095.appspot.com/o/best_model.hdf5?alt=media&token=124965a7-56ae-43c8-ac90-b99220dde13d'
-        r = requests.get(url, allow_redirects=True)
-        with open('my_model.hdf5', 'wb') as f_hdf5, requests.Session() as req:
-            f_hdf5.write(r.content)
+        word_segment_url = word_segmentation_model_url
+        response = requests.get(word_segment_url, allow_redirects=True)
+        with open("my_model.hdf5", "wb") as f_hdf5, requests.Session() as req:
+            f_hdf5.write(response.content)
 
-            resp = req.get('https://firebasestorage.googleapis.com/v0/b/ysc-kme-25095.appspot.com/o/vocab.json?alt=media&token=34178ef2-9e62-491f-9fc5-bf2e30a33635')
+            resp = req.get(word_segmentation_model_url)
             self.CHAR_INDICES = resp.json()
-        
-        self.model = tf.keras.models.load_model('my_model.hdf5')
-        os.remove('my_model.hdf5')            
-        
+
+        self.model = tfk.models.load_model("my_model.hdf5")
+        os.remove("my_model.hdf5")
+
         self.look_back = 5
-        
+
     def create_dataset(self, text):
         """
-        take text with label (text that being defined where to cut ('|')) 
+        take text with label (text that being defined where to cut ('|'))
         and encode text and make label
         return preprocessed text & preprocessed label
         """
         X, y = [], []
-        text = '|' + text
-        data = [self.CHAR_INDICES['<pad>']] * self.look_back
+        text = "|" + text
+        data = [self.CHAR_INDICES["<pad>"]] * self.look_back
         for i in range(1, len(text)):
             current_char = text[i]
-            before_char = text[i-1]
+            before_char = text[i - 1]
 
-            if current_char == '|':
+            if current_char == "|":
                 continue
             data = data[1:] + [self.CHAR_INDICES[current_char]]  # X data
 
-            target = 1 if before_char == '|' else 0  # y data
+            target = 1 if before_char == "|" else 0  # y data
             X.append(data)
             y.append(target)
-        
+
         return np.array(X), tf.one_hot(y, 2)
-        
+
     def preprocessing_text(self, raw_text):
         """
         take unseen (testing) text and encode it with CHAR_DICT
@@ -48,9 +53,11 @@ class Segmentation():
         return preprocessed text
         """
         X = []
-        data = [self.CHAR_INDICES['<pad>']] * self.look_back
+        data = [self.CHAR_INDICES["<pad>"]] * self.look_back
         for char in raw_text:
-            char = char if char in self.CHAR_INDICES else '<unk>'  # check char in dictionary
+            char = (
+                char if char in self.CHAR_INDICES else "<unk>"
+            )  # check char in dictionary
             data = data[1:] + [self.CHAR_INDICES[char]]  # X data
             X.append(data)
         return np.array(X)
@@ -58,7 +65,7 @@ class Segmentation():
     def predict(self, preprocessed_text):
         pred = self.model.predict(preprocessed_text)
         class_ = tf.argmax(pred, axis=-1).numpy()
-        
+
         return class_
 
     def word_segmentation(self, text):
@@ -67,23 +74,27 @@ class Segmentation():
         class_[0] = 1
         class_ = np.append(class_, 1)
 
-        cut_indexs = [i for i, value in enumerate(class_) if value == 1]
-        words = [text[cut_indexs[i]:cut_indexs[i+1]] for i in range(len(cut_indexs)-1)]
-        
-        join_word = '|'.join(words)
-        
+        cut_indexes = [i for i, value in enumerate(class_) if value == 1]
+        words = [
+            text[cut_indexes[i] : cut_indexes[i + 1]]
+            for i in range(len(cut_indexes) - 1)
+        ]
+
+        join_word = "|".join(words)
+
         return words, join_word
+
 
 class Tokenizer:
     def __init__(self):
-        self.word2index = {"<pad>": 0, "<start>": 1,"<end>": 2, "<unk>": 3}
-        self.word2count = {"<pad>": 0, "<start>": 0,"<end>": 0, "<unk>": 0}
+        self.word2index = {"<pad>": 0, "<start>": 1, "<end>": 2, "<unk>": 3}
+        self.word2count = {"<pad>": 0, "<start>": 0, "<end>": 0, "<unk>": 0}
         self.index2word = {0: "<pad>", 1: "<start>", 2: "<end>", 3: "<unk>"}
         self.num_words = 4
         self.num_sentences = 0
         self.longest_sentences = 0
         self.kme_segment = Segmentation()
-    
+
     def add_word(self, word):
         if word not in self.word2index:
 
@@ -96,24 +107,24 @@ class Tokenizer:
 
             # Word exists; increase word count
             self.word2count[word] += 1
-    
+
     def fit_on_text(self, sentences):
         for sentence in sentences:
 
-            # Model predict 
+            # Model predict
             segmented_text, _ = self.kme_segment.word_segmentation(sentence)
 
             # Add <start> at start and <end> at end of each sentences
-            segmented_text = np.concatenate((['<start>'], segmented_text, ['<end>']))
+            segmented_text = np.concatenate((["<start>"], segmented_text, ["<end>"]))
 
             for word in segmented_text:
-                self.add_word(word)       
-    
+                self.add_word(word)
+
     def text_to_sequences(self, sentences, method_pad=None):
         sequences_arr = []
         for sentence in sentences:
 
-            # Model predict 
+            # Model predict
             segmented_text, _ = self.kme_segment.word_segmentation(sentence)
             tokenize_text_arr = []
 
@@ -122,16 +133,18 @@ class Tokenizer:
                     tokenize_text_arr.append(self.word2index[word])
                 except KeyError:
 
-                    # use <unk> key for word that never met 
-                    tokenize_text_arr.append(self.word2index['<unk>'])
+                    # use <unk> key for word that never met
+                    tokenize_text_arr.append(self.word2index["<unk>"])
 
             sequences_arr.append(tokenize_text_arr)
         if method_pad:
             # Make zero padding by Maximum length of sentence
-            sequences_padded = tf.keras.preprocessing.sequence.pad_sequences(sequences_arr, padding=method_pad)
+            sequences_padded = tfk.preprocessing.sequence.pad_sequences(
+                sequences_arr, padding=method_pad
+            )
             return sequences_padded
         return sequences_arr
-    
+
     def sequences_to_text(self, sequences):
         texts_arr = []
         for sequence in sequences:
@@ -140,4 +153,3 @@ class Tokenizer:
                 index_arr.append(self.index2word[index])
             texts_arr.append(index_arr)
         return texts_arr
-                
